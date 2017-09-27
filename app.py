6868918af94c34865c2e61f40bde5d9889b3da24
ppark3 <ppark3@stuy.edu>
2017-09-26 20:07:59 -0400

    completed assignment

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
 from flask import Flask, render_template
-from occupy import reader, choosejob()
+from occupy import reader, choosejob
 
 app = Flask(__name__)
 
+@app.route('/')
+def main():
+	return "Add '/occupations' and you might find what you're looking for."
+
 @app.route('/occupations')
 def display():
-    return render_template('template.html')
-
+    return render_template('template.html', dict = reader('occupations.csv'), random = choosejob('occupations.csv'))
+	
 if __name__ == '__main__':
     app.run()
