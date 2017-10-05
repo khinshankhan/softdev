891a9c50bee2bdbde8168e154ba9e5ab430c7180
ppark3 <ppark3@stuy.edu>
2017-10-04 23:05:53 -0400

    edited some stuff

--- app.py
+++ app.py
@@ -5,30 +5,37 @@ app = Flask(__name__)
 app.secret_key = 'keysmithsmakekeys'
 
 username = 'admin'
-password = 'this isnt a password'
+password = 'password'
 
 @app.route('/')                                                                 
 def root():
     #return  'GO TO 127.0.0.1:5000/login TO LOG IN'
     if 'user' in session:
+        print session
         return redirect('/welcome')
+    print session
     return redirect('/login')
 
-@app.route('/login')
+@app.route('/login', methods=['POST', 'GET'])
 def login():
+    print session
     return render_template("template.html")
 
-@app.route('/welcome', methods=['POST'])
+@app.route('/welcome', methods=['POST', 'GET'])
 def welcome():
     #DEBUGGING, SOMETHING IS WRONG HERE...
     if 'user' in session:
         print "HEUUUUUUUUUUUUUUUUUUUUUUUUUUUUUWYYYYYYYYYYYYYYYYYY"
     else:
         print "OOOOOOOOOOOOOOOOOOOOOHHHHHHHHHHHH NOOOOOOOOOOOOOOO"
-        #return render_template('welcome.html')
+    print request.form
+    print session
     if request.form['user'] == username and request.form['pass'] == password:
-        session['user'] = 'user'
-        return render_template('welcome.html')
+        if 'user' in session:
+		    return render_template('welcome.html')
+        else:
+            session['user'] = 'data'
+            return render_template('welcome.html')
     else:
         wrong = 'SORRY, BUT YOUR '
         if request.form['user'] != username and request.form['pass'] != password:
@@ -40,8 +47,9 @@ def welcome():
                 wrong += 'PASSWORD'
         return render_template('badlogin.html', problem = wrong+' IS INCORRECT, PLEASE TRY AGAIN')
 
-@app.route('/logout')
+@app.route('/logout', methods=['POST', 'GET'])
 def logout():
+   print session
    session.pop('user')
    return redirect('/login')
 
