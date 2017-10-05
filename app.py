f9197c3a1f068406d40ed22b8769273744c3f36c
Khinshan <kkhan@stuy.edu>
2017-10-04 20:55:58 -0400

    log out not working

--- app.py
+++ app.py
@@ -1,29 +1,42 @@
-from flask import Flask, render_template, request, session
+from flask import Flask, render_template, request, session, redirect
 
 app = Flask(__name__)
 
-username = 'asdf'
+app.secret_key = 'keysmithsmakekeys'
+
+username = 'admin'
 password = 'this isnt a password'
 
 @app.route('/')                                                                 
+def root():
+    #return  'GO TO 127.0.0.1:5000/login TO LOG IN'
+    return redirect('/login')
+
+@app.route('/login')
 def login():
     return render_template("template.html")
 
-@app.route('/login')
-def next():
+@app.route('/welcome', methods=['POST'])
+def welcome():
     if request.form['user'] == username and request.form['pass']== password:
-        app.secret_key=username
-        session[username]='appsession'
-        if 'log' in request.args:
-            session.pop(username)
+        session['username'] = request.form['user']
         return render_template('welcome.html')
     else:
-        if request.form['user'] != username and not request.form['pass']== password:
-            wrong = 'USERNAME AND PASSWORD'
+        wrong = 'SORRY, BUT YOUR '
+        if request.form['user'] != username and request.form['pass'] != password:
+            wrong += 'USERNAME AND PASSWORD'
         else:
-            if +
-        return render_template('badlogin.html', wrong)
-    
+            if request.form['user'] != username:
+                wrong += 'USERNAME'
+            else:
+                wrong += 'PASSWORD'
+        return render_template('badlogin.html', problem = wrong+' IS INCORRECT, PLEASE TRY AGAIN')
+
+@app.route('/logout')
+def logout():
+   session.pop('user', None)
+   return redirect('/login')
+
 if __name__ == '__main__':
     app.debug = True
     app.run()
