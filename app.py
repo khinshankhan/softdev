bac0ea6835abc1b525cf685e94ddf91aef39d040
Khinshan <kkhan@stuy.edu>
2017-10-12 21:25:47 -0400

    restructured and flashing. need to work out kink with user directly typing /welcome

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, session, redirect, url_for
+from flask import Flask, render_template, request, session, redirect, url_for, flash
 
 app = Flask(__name__)
 
@@ -19,38 +19,45 @@ def login():
 	#if session is already in place, go to welcome.html right away
 	#otherwise, go to login page
 	if 'user' in session:
-	    return render_template('welcome.html')
+	    return redirect(url_for('welcome'))
 	else:
 	    return render_template("template.html")
 
+#checks
+#will say what's wrong
+def authenticate(user, passw):
+    if username == user:
+        if password == passw:
+            return 'good'
+        else:
+            return 'password'
+    else:
+        if password == passw:
+            return 'username'
+        else:
+            return 'username and password'
+
 @app.route('/welcome', methods=['POST', 'GET'])
 def welcome():
-	#if session is already in place, go to welcome.html without asking for request
-    if 'user' in session:
-        print "HEUUUUUUUUUUUUUUUUUUUUUUUUUUUUUWYYYYYYYYYYYYYYYYYY"
-        return render_template('welcome.html')
-    else:
-        print "OOOOOOOOOOOOOOOOOOOOOHHHHHHHHHHHH NOOOOOOOOOOOOOOO"
-		
-    #if session was not in place, use the info inputted by the user to go to welcome.html
-    if request.form['user'] == username and request.form['pass'] == password:
-        if 'user' in session:
-		    return render_template('welcome.html')
-        else:
-            session['user'] = 'data'
-            return render_template('welcome.html')
-			
-	#if the username, password, or both were wrong
-    else:
-        wrong = 'SORRY, BUT YOUR '
-        if request.form['user'] != username and request.form['pass'] != password:
-            wrong += 'USERNAME AND PASSWORD'
-        else:
-            if request.form['user'] != username:
-                wrong += 'USERNAME'
+    if 'user' not in session:
+        print 'NOT LOGGED IN'
+        if request.args != []:
+            #print request.args
+            print 'REQUESTS MADE'
+            user = request.form['user']
+            passw = request.form['pass']
+        
+            result = authenticate(user, passw)
+        
+            if result == 'good':
+                session['user'] = user
             else:
-                wrong += 'PASSWORD'
-        return render_template('badlogin.html', problem = wrong+' IS INCORRECT, PLEASE TRY AGAIN')
+                flash ('Sorry, but your '+ result + ' is wrong. Please try again')
+        print 'FINALE!'
+        return redirect(url_for('login'))
+
+    else:
+        return render_template('welcome.html', name = session['user'])
 
 @app.route('/logout', methods=['POST', 'GET'])
 def logout():
