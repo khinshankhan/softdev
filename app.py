11cc80a0b295e8f8f79bd80c7f60e923765598dc
Khinshan Khan <kkhan@stuy.edu>
2017-11-11 12:30:54 -0500

    made selection screen to incorporate random user api

--- app.py
+++ app.py
@@ -11,8 +11,13 @@ def eprint(*args, **kwargs):
     print(*args, file=sys.stderr, **kwargs)
     return
 
+#choose project
 @app.route('/')                                                                 
 def root():
+    return render_template('index.html')
+
+@app.route('/nasa')                                                                 
+def nasa():
     key = 'Y2Fj9o8eCYnHgVCYuMvKtSi8q7Kgufqgc09mJRIR'
     url = 'https://api.nasa.gov/planetary/apod?api_key={}'.format(key)
     data = urllib2.urlopen(url).read()
@@ -25,7 +30,24 @@ def root():
         eprint('ENTRY: %s'%d[i])
         eprint("")
     '''
-    return render_template('index.html', d = d)
+    return render_template('nasa.html', d = d)
+
+@app.route('/randomuser')                                                                 
+def randomuser():
+    #documentation: https://randomuser.me/
+    url = 'https://randomuser.me/api/?format=json'
+    data = urllib2.urlopen(url).read()
+    d = json.loads(data)
+    #debugging
+    #eprint(d['results'][0])
+    '''
+    for i in d:
+        eprint('TITLE: %s'%i)
+        eprint("")
+        eprint('ENTRY: %s'%d[i])
+        eprint("")
+    '''
+    return render_template('randomuser.html', d = d)
 
 if __name__ == '__main__':
     app.debug = True
