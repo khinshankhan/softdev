1553b70641556ad649eaf9b99c9c996755383c01
Khinshan Khan <kkhan@stuy.edu>
2017-12-08 15:15:02 -0500

    first bullet done

--- intro.js
+++ intro.js
@@ -1,5 +1,16 @@
 var add = function(e){
-    console.log('HEY');
+    var ol = document.getElementById("thelist");
+    var li = document.createElement("li");
+    var num = document.getElementsByTagName("li").length;
+    li.innerHTML = "item " + num;
+    ol.appendChild(li);
 };
 
 var button = b.addEventListener('click', add);
+
+var change = function(e){
+    var position = this;
+    console.log(position);
+};
+
+var head = ol.addEventListener('hover', change);
