63ff5e750c94b0af6b440c73064abed9e68a9b3a
Khinshan Khan <kkhan@stuy.edu>
2018-02-08 19:36:32 -0500

    old-timie DVD animation!

--- canvas.js
+++ canvas.js
@@ -25,8 +25,11 @@ var toggle = function () {
 var animation = function () {
     var radius = 0;
     action = 1;
+
+    window.cancelAnimationFrame(requestID);
     
-    var circle = function () {
+    //grow n shrink circle
+    var gs = function () {
 	clear();
 	ctx.beginPath();
 	ctx.arc(c.width/2, c.height/2, radius, 0, 2*Math.PI);
@@ -45,10 +48,10 @@ var animation = function () {
 	if(radius == 0 || radius == c.height/2){
 	    toggle();
 	}
-	console.log(requestID);
-	requestID = window.requestAnimationFrame(circle);
+	//console.log(requestID);
+	requestID = window.requestAnimationFrame(gs);
     };
-    circle();
+    gs();
 };
 
 //start animation + save requestID
@@ -67,3 +70,47 @@ stahp.addEventListener('click', stopit);
 //connect animation fxn to reset button, in a sense restarting it
 reset.addEventListener('click', animation);
 
+//animation to move like old-timie DVD symbol
+var dvda = function () {
+    // want it to be 50-450 for both
+    var x = Math.floor(Math.random() * (c.width - 100)) + 50;
+    var y = Math.floor(Math.random() * (c.height - 100)) + 50;
+    //random pos or neg direction for start
+    var dx = Math.random() < 0.5 ? -1 : 1;
+    var dy = Math.random() < 0.5 ? -1 : 1;
+
+    console.log("X: "+dx);
+    console.log("Y: "+dy);
+    
+    window.cancelAnimationFrame(requestID);
+    
+    //bounce bounce! like a DVD symbol!
+    var bounce = function () {
+	clear();
+	ctx.beginPath();
+	ctx.arc(x, y, 25, 0, 2*Math.PI);
+	//sets the color
+	color = document.getElementById("colors").value;
+	ctx.strokeStyle = color;
+	ctx.stroke();
+	ctx.fillStyle = color;
+	ctx.fill();
+
+	if(x == 25 || x == c.width - 25){
+	    dx = dx * -1;
+	}
+	if(y == 25 || y == c.height - 25){
+	    dy = dy * -1;
+	}
+	
+	x += dx;
+	y += dy;
+
+	
+	//console.log(requestID);
+	requestID = window.requestAnimationFrame(bounce);
+    };
+    bounce();
+};
+
+dvd.addEventListener('click', dvda);
