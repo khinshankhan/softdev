5d23d45ca3afaf510ac4fef253cd26d17eff765d
Jake Zaia <jzaia@stuy.edu>
2018-03-25 13:51:27 -0400

    Basic layout

---
+++ data.js
@@ -0,0 +1,13 @@
+var graph = document.getElementById("data");
+var button = document.getElementById("nextGraph");
+
+var data1 = { year: 2018,
+              deficit_percent: -2.3,
+              deficit_amount: -392,
+              gdp: 17.4,
+              totalDebt: 18};
+var data2 = { year: 2017,
+              deficit_percent: -2.6,
+              deficit_amount: -443,
+              gdp: 17,
+              totalDebt: 17.7};
